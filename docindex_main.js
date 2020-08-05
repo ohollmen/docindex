2cc9fc2b187c3ddb6abd95acaa2bcb985ab6d08c
Olli Hollmen <olli.hollmen@gmail.com>
2020-08-05 07:42:35 -0700

    Add example of using ondocchange event/callback.

--- docindex_main.js
+++ docindex_main.js
@@ -1,7 +1,8 @@
 // $(document).ready(function ()
 window.onload = function () {
   // Instantiate docIndex. Load index "TOC" with initdocs() method.
-  var cfg = new docIndex({acc: 0, linkproc: "post"});
+  docIndex.ondocchange = function (docurl) { console.log("DOC-CHANGE: "+docurl); }
+  var cfg = new docIndex({acc: 0, linkproc: "post", settitle: 1, debug: 1});
   // TODO: cfg.filename
   $.getJSON("docindex.json", function (d) { cfg.initdocs(d); });
 };
