d8f433b1f018faac4d8da3e99d2959a847a62a98
Olli Hollmen <olli.hollmen@gmail.com>
2021-03-17 08:51:47 -0700

    Add semicolon with hint from jshint.

--- docindex_main.js
+++ docindex_main.js
@@ -1,7 +1,7 @@
 // $(document).ready(function ()
 window.onload = function () {
   // Instantiate docIndex. Load index "TOC" with initdocs() method.
-  docIndex.ondocchange = function (docurl) { console.log("DOC-CHANGE: "+docurl); }
+  docIndex.ondocchange = function (docurl) { console.log("DOC-CHANGE: "+docurl); };
   var cfg = new docIndex({acc: 0, linkproc: "post", settitle: 1, debug: 1, nosidebarhide: 1});
   // TODO: cfg.filename
   $.getJSON("docindex.json", function (d) { cfg.initdocs(d); });
