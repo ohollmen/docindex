20a49111b9b764e0d5e213547e26b1e40319ef64
Olli Hollmen <olli.hollmen@gmail.com>
2024-02-17 23:57:52 -0800

    Default to internal doc loader.

--- docindex_main.js
+++ docindex_main.js
@@ -4,5 +4,7 @@ window.onload = function () {
   docIndex.ondocchange = function (docurl) { console.log("DOC-CHANGE: "+docurl); };
   var cfg = new docIndex({acc: 0, linkproc: "post", settitle: 1, debug: 1, nosidebarhide: 1});
   // TODO: cfg.filename
-  $.getJSON("docindex.json", function (d) { cfg.initdocs(d); });
+  // Original JQuery way
+  // $.getJSON("docindex.json", function (d) { cfg.initdocs(d); });
+  docIndex.htget("docindex.json", function (d) { cfg.initdocs(d); });
 };
