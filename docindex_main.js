479e5047be27234d306cd0c9b5ef6839e5d8437d
Olli Hollmen <olli.hollmen@gmail.com>
2026-02-23 12:40:37 -0800

    Enable tables by default. Add docnotes.

--- docindex_main.js
+++ docindex_main.js
@@ -1,10 +1,12 @@
 // $(document).ready(function ()
 window.onload = function () {
   // Instantiate docIndex. Load index "TOC" with initdocs() method.
-  docIndex.ondocchange = function (docurl) { console.log("DOC-CHANGE: "+docurl); };
+  docIndex.ondocchange = function (docurl) { console.log(`DOC-CHANGE: ${docurl}`); };
   var cfg = new docIndex({acc: 0, linkproc: "post", settitle: 1, debug: 1, nosidebarhide: 1});
   // TODO: cfg.filename
-  // Original JQuery way
+  // Modernization - support tables (even if not part of orginal MD spec, these are nearly a "must have")
+  docIndex.converter.setOption('tables', true);
+  // Original JQuery way (still works, but default to not depending on JQuery)
   // $.getJSON("docindex.json", function (d) { cfg.initdocs(d); });
   docIndex.htget("docindex.json", function (d) { cfg.initdocs(d); });
 };
